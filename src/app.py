2d125c634c33e3af5812566edcbb38759e5d8596
sharon refael <sharon@mirage-web.com>
2026-01-10 21:59:57 +0200

    push docker files

---
+++ src/app.py
@@ -0,0 +1,23 @@
+#'/api/v1/details'
+#'/api/v1/healthz'
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask(__name__)
+
+@app.route('/api/v1/details')
+def details():
+    return jsonify({
+        'time': '' + datetime.datetime.utcnow().isoformat() + 'Z',
+        'hostname': '' + socket.gethostname() + ''
+    })
+
+@app.route('/api/v1/healthz')
+def health():
+    return jsonify({
+        'status': 'up'
+    }), 200
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
